948c4243c22af11affeeda5ff5399cf97743a4bd
Pavel823 <pashandreev4@gmail.com>
2024-12-16 23:17:50 +0300

    footer, header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,23 @@
+﻿import { NavLink, useLocation, useNavigate } from 'react-router-dom';
+
+export const Header = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const authClassName = location.pathname === '/login' ? 'isActive' : '';
+
+
+
+  
+  return (
+    <header className="header">
+      <NavLink to="/">Главная</NavLink>
+      <NavLink to="/users">Пользователи</NavLink>
+      <NavLink to="/posts">Посты</NavLink>
+      <div
+        className={`btn_auth ${authClassName}`}
+      >
+      </div>
+    </header>
+  )
+} 
---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,16 @@
+﻿import { Outlet } from 'react-router-dom';
+
+import { Header } from '../header/Header';
+import { Footer } from '../footer/Footer';
+
+const Layout = () => {
+    return (
+      <div className='wrapper'>
+        <Header />        
+        <main className="main"><Outlet /></main>
+        <Footer/>
+      </div>
+    )
+}
+
+export {Layout}
