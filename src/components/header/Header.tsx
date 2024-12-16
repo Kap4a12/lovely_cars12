a296f1d6546a29825d3899ee7c076de56667e439
Pavel823 <pashandreev4@gmail.com>
2024-12-17 00:41:32 +0300

    services

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,18 @@
 ﻿import { NavLink, useLocation, useNavigate } from 'react-router-dom';
+import { useAuth } from '../hook/useAuth';
 
 export const Header = () => {
+  const auth = useAuth();
   const navigate = useNavigate();
   const location = useLocation();
 
   const authClassName = location.pathname === '/login' ? 'isActive' : '';
 
-
+  const handleAuthToggle = () => {
+    auth?.user
+      ? auth?.signout(() => navigate('/', { replace: true }))
+      : navigate('/login')
+  };
 
   
   return (
@@ -16,7 +22,12 @@ export const Header = () => {
       <NavLink to="/posts">Посты</NavLink>
       <div
         className={`btn_auth ${authClassName}`}
+        onClick={handleAuthToggle}
       >
+        {auth?.user
+          ? 'Выйти'
+          : 'Войти'
+        }
       </div>
     </header>
   )
